f0163c828d3cf38fd1e06ed6bd242dfa05f5fd28
DennisdeBest <dennis@debest.fr>
2016-03-23 15:45:45 +0100

    Routes and parameters

--- serveur.js
+++ serveur.js
@@ -1,8 +1,19 @@
 var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
 
 var server = http.createServer(function (req, res) {
+    var page = url.parse(req.url).pathname;
+    var params = querystring.parse(url.parse(req.url).query);
+    console.log(page);
     res.writeHead(200, {"Content-Type":"text/html"});
-    res.end('<h1>NodeJS Server html</h1>');
+    if('prenom' in params && 'nom' in params) {
+        res.write('Salut' + params['prenom'] + ' ' + params['nom']);
+    }
+    else {
+        res.write('Salut l\'inconnue');
+    }
+    res.end();
 
 });
 
